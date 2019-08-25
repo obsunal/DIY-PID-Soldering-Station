e72448a814be522d0c870929c60b3f6232058186
obsunal <akirpi41@gmail.com>
2019-08-25 19:58:44 +0300

    [Add] Worlds Worst PWM Power Regulation
    I am not sure 244 Hz PWM enough for power regulation but I got 2 16 bit PWMs running. That's something, right? Riiiiight?3

--- main.c
+++ main.c
@@ -8,15 +8,17 @@
 #include <avr/io.h>
 #include <stdlib.h>
 #include <avr/interrupt.h>
-#define F_CPU 8000000
+#define F_CPU 16000000
 #define menu_key PD5
 #define down_key PD6
 #define up_key  PD7
+#define pwm_pin1 PB1
+#define pwm_pin2 PB2
 
 //values for PID
-uint8_t pwm_value;
+uint16_t pwm_value;
 int current_pos;
-	
+
 int goal = 270;
 int idle = 50;
 int target = 270;
@@ -31,19 +33,29 @@ double ki;
 double kd;
 
 int read_pos();
-void set_pwm(int8_t pwm_value);
+void set_pwm(uint16_t pwm_value);
+
+bool is_idle = false;
 
 char state = "Booting...";
 
 int main(void) {
-	DDRB |= (1 << DDB3);	//Set PWM pin output
+	DDRB = 0b00000110;		//PB1 & PB2 Output
+	DDRD = 0b00000000;		//Interrupts and Buttons Input
+	PORTD = 0b11100100;		//Pull-Ups
 	
-	DDRD &= ~(1 << DDD2) & ~(1 << DDD5) & ~(1 << DDD6) & ~(1 << DDD7);	//Interrupt 1 and ok key, up key, down key  input
-	PORTD |= (1 << PD2) & ~(1 << menu_key) & ~(1 << down_key) & ~(1 << up_key);		//Interrupt 1 and PD5 pull-up
-	
-	MCUCR |= (1 << ISC00);	//make interrupt 0's mode logic change
-	GICR |= (1 << INT0);	//Assign Interrupt 0
+	EICRA = 0b00000001;		//make interrupt 0's mode logic change
+	EIMSK = 0b00000001;		//Assign Interrupt 0
 	sei();					//Starts Interrupts
+	
+	ICR1 = 0xFFFF;			//Set TOP value for fast PWM
+	OCR1A = 0x0000;			//Clear PB1 PWM
+	OCR1B = 0x0000;			//Clear PB2 PWM
+	
+	TCCR1B = 0b00011001;	//Enable timer1 with 0 pre-scaler and set 2 bits of waveform generation mode
+	TCCR1A = 0b10100010;	//Set Compare mode and wave generation mode
+	ICR1 = 0xFFFF;			//Set Top value.
+	
 	
 	state = "Ready!";
 	
@@ -59,19 +71,14 @@ int main(void) {
 		
 		pwm_value = (kp * p_error) + (ki * i_error) + (kd * d_error);
 		
-		set_pwm(pwm_value);
+		OCR1A = pwm_value;		//sets the PWM value;
 		
 		last_p = p_error;
     }
 }
 
 ISR (INT0_vect) {
-	if((PIND & (1 << PIND2)) == 0) {
-		goal = idle;
-	}
-	else {
-		goal = target;
-	}
+	is_idle = (PIND >> PIND2);
 }
 
 int read_pos() {
@@ -84,8 +91,4 @@ int read_pos() {
 	adc_value = ADCL;					//Can be combined with next line but
 	adc_value = (ADCH << 8) + adc_value	//IDK how compiler works, so don't want to loose reading
 	return adc_value;					//Because of compiler optimizations.
-}
-
-void set_pwm(int8_t pwm_value) {
-	
 }
